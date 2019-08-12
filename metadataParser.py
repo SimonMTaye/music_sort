2b5335912d96b2304bf3fac3f8a1de2357e468f4
SirPotato10000 <mulat.simon@gmail.com>
2019-08-12 18:50:42 -0400

    Refactored

--- metadataParser.py
+++ metadataParser.py
@@ -1,10 +1,8 @@
 from tinytag import TinyTag
 from os.path import basename
 
-class metadataParser:
-
-    def parseSong(self, songPath: str):
-        metadata = TinyTag.get(songPath)
-        metadata.path = songPath
-        metadata.name = basename(songPath)
-        return metadata
+def parseSong(songPath: str):
+    metadata = TinyTag.get(songPath)
+    metadata.path = songPath
+    metadata.name = basename(songPath)
+    return metadata
