8d771c2ffd00335f40f593ffccd3daabbe0e11f9
SirPotato10000 <mulat.simon@gmail.com>
2019-08-16 20:40:03 -0400

    Added Duplicate manager class

---
+++ music_sort/duplicateManager.py
@@ -0,0 +1,30 @@
+import fuzzywuzzy, shutil
+
+## If audio fingerprinting is ever implemented, use that for comparisons
+
+class duplicateManager:
+
+    def _init_(self, songList):
+        self.songList = songList
+        self.duplicateSongList = []
+
+    def checkIfDuplicate(self, newSong):
+        for index, oldSong in enumerate(self.songList):
+            titleSimilarity = fuzzywuzzy.fuzz.token_set_ratio(oldSong.title, newSong.title)
+            artistSimilarity = fuzzywuzzy.fuzz.token_set_ratio(oldSong.artist, newSong.artist)
+            if((artistSimilarity + titleSimilarity) / 2 > 0.85 ):
+                self.handleDuplicate(newSong, index)
+        ## -1 Duplicate value shows song doesn't exist in list        
+        return ({'isDuplicate': False, 'index': -1})
+
+    def handleDuplicate (self, duplicateSong, originalSongIndex):
+        originalSong = self.songList[originalSongIndex]
+        if(originalSong.bitrate == 320 or originalSong.bitrate >= duplicateSong.bitrate ):
+            self.duplicateSongList.append(duplicateSong)
+        else:
+            self.duplicateSongList.append(originalSong)
+            del self.songList[originalSongIndex]
+
+    def sortDuplicates(self):
+        for duplicate in self.duplicateSongList:
+            shutil.move(duplicate.path, 'duplicates')           
