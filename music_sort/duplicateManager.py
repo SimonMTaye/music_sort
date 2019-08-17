dd452e880acd121500004faeb27dd552f1085384
SirPotato10000 <mulat.simon@gmail.com>
2019-08-17 02:15:16 -0400

    Refactored

--- music_sort/duplicateManager.py
+++ music_sort/duplicateManager.py
@@ -6,25 +6,24 @@ class duplicateManager:
 
     def __init__(self, songList):
         self.songList = songList
+        self.checkedSongList = []
         self.duplicateSongList = []
 
-    def checkIfDuplicate(self, newSong):
-        for index, oldSong in enumerate(self.songList):
-            titleSimilarity = fuzzywuzzy.fuzz.token_set_ratio(oldSong.title, newSong.title)
-            artistSimilarity = fuzzywuzzy.fuzz.token_set_ratio(oldSong.artist, newSong.artist)
-            if((artistSimilarity + titleSimilarity) / 2 > 0.85 ):
-                self.handleDuplicate(newSong, index)
-                return index
-        ## -1 Duplicate value shows song doesn't exist in list        
-        return -1
+    def checkForDuplicates(self):
+        for uncheckedSong in self.songList:
+            for index, song in enumerate(self.checkedSongList):
+                titleSimilarity = fuzzywuzzy.fuzz.token_set_ratio(uncheckedSong.title, song.title)
+                artistSimilarity = fuzzywuzzy.fuzz.token_set_ratio(uncheckedSong.artist, song.artist)
+                if((artistSimilarity + titleSimilarity) / 2 > 0.85 ):
+                    self.handleDuplicate(uncheckedSong, index)
 
     def handleDuplicate (self, duplicateSong, originalSongIndex):
-        originalSong = self.songList[originalSongIndex]
+        originalSong = self.checkedSongList[originalSongIndex]
         if(originalSong.bitrate == 320 or originalSong.bitrate >= duplicateSong.bitrate ):
             self.duplicateSongList.append(duplicateSong)
         else:
             self.duplicateSongList.append(originalSong)
-            del self.songList[originalSongIndex]
+            del self.checkedSongList[originalSongIndex]
 
     def sortDuplicates(self):
         for duplicate in self.duplicateSongList:
