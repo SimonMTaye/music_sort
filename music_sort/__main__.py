b6d1895a3fd19a87acda11544a05ec85773aa266
SirPotato10000 <mulat.simon@gmail.com>
2019-08-17 02:22:31 -0400

    Implemented CLI and ability to use music_sort. Testing required

---
+++ music_sort/__main__.py
@@ -0,0 +1,28 @@
+import fire
+
+from . import duplicateManager, metadataParser, pathSorter, fileScanner
+
+def sortMusic(dir, recursive=True, sortingProperties=('artist', 'album'), musicFileTypes=['mp3','m4a','flac']):
+    for property in sortingProperties:
+        if property not in ['artist', 'genre','album', 'bitrate', 'albumartist']:
+            raise ValueError('Unsupported value used as sorting property. Use: "artist", "genre", "album", "bitrate" or "albumartist"')
+    for type in musicFileTypes:
+        if type not in ['mp3', 'm4a', 'flac','ogg','wav']:
+            raise ValueError('Unsupported music file type. Use: mp3, m4a, flac, ogg or wav')  
+    if recursive:
+        scannedFiles = fileScanner.scanFolderRecursively(dir, musicFileTypes)
+    elif not recursive:
+        scannedFiles = fileScanner.scanFolder(dir, musicFileTypes)
+    parsedSongs = []    
+    for file in scannedFiles:
+        metadata = metadataParser.parseSong(file)
+        parsedSongs.append(metadata)
+    duplicateMan = duplicateManager.duplicateManager(parsedSongs)
+    duplicateMan.checkForDuplicates()
+    for song in parsedSongs:
+        pathMan = pathSorter.pathSorter(sortingProperties, song)
+        pathMan.createDir()
+        pathMan.moveSong()
+
+if __name__ == '__main__':
+    fire.Fire(sortMusic)        
