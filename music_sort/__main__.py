8a2439a3450a9d36fab2f3447d5e0941d7aa3272
SirPotato10000 <mulat.simon@gmail.com>
2019-08-25 09:54:03 -0400

    Added duration measuring and groundwork for multiprocessing

--- music_sort/__main__.py
+++ music_sort/__main__.py
@@ -1,8 +1,10 @@
-import fire
+import fire, time
 
 import duplicateManager, metadataParser, pathSorter, fileScanner
 
-def sortMusic(dir=r'C:\Users\smtsi\Code\Test', recursive=True, sortingProperties=('artist', 'album'), musicFileTypes=['mp3','m4a','flac']):
+##Potentially add multiprocessing
+
+def sortMusic(dir=r'C:\Users\smtsi\Code\Test', recursive=True, sortingProperties=('artist', 'album'), musicFileTypes=['mp3','m4a','flac'], checkForDuplicate=True):
     for property in sortingProperties:
         if property not in ['artist', 'genre','album', 'bitrate', 'albumartist']:
             raise ValueError('Unsupported value used as sorting property. Use: "artist", "genre", "album", "bitrate" or "albumartist"')
@@ -10,22 +12,35 @@ def sortMusic(dir=r'C:\Users\smtsi\Code\Test', recursive=True, sortingProperties
         if type not in ['mp3', 'm4a', 'flac','ogg','wav']:
             raise ValueError('Unsupported music file type. Use: mp3, m4a, flac, ogg or wav')  
     if recursive:
+        start = time.time()
         scannedFiles = fileScanner.scanFolderRecursively(dir, musicFileTypes)
+        end = time.time()
+        print("File scanning took: " + str(end - start))
     elif not recursive:
         scannedFiles = fileScanner.scanFolder(dir, musicFileTypes)
-    parsedSongs = []    
+    parsedSongs = []
+    start = time.time()        
     for file in scannedFiles:
         metadata = metadataParser.parseSong(file)
         parsedSongs.append(metadata)
+    end = time.time()
+    print("Parsing songs took: " + str(end - start))    
     duplicateMan = duplicateManager.duplicateManager(parsedSongs, dir)
-    duplicateMan.checkForDuplicates()
+    if(checkForDuplicate):
+        start = time.time()
+        duplicateMan.checkForDuplicates()
+        end = time.time()
+        print("Duplicate sorting took: " + str(end - start))
+    else:
+        duplicateMan.skipDuplicateChecking()    
     duplicateMan.handleDuplicates()
-    duplicateMan.sortDuplicates()
     parsedSongs = duplicateMan.checkedSongList
+    start = time.time()
     for song in parsedSongs:
         pathMan = pathSorter.pathSorter(sortingProperties, song, dir)
         pathMan.createDir()
         pathMan.moveSong()
-
+    end = time.time()
+    print("Moving songs took: " + str(end - start))
 if __name__ == '__main__':
     fire.Fire(sortMusic)        
