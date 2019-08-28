4c11ae9d2857fe3ded5471f7a4e66bf161ee4734
SirPotato10000 <mulat.simon@gmail.com>
2019-08-28 16:42:22 +0300

    Fixed some bugs. Further ground work for multiprocessing

--- music_sort/duplicateManager.py
+++ music_sort/duplicateManager.py
@@ -1,39 +1,51 @@
 import shutil, os
-import multiprocessing
+import multiprocessing, pickle
 from fuzzywuzzy import fuzz
 
 ## If audio fingerprinting is ever implemented, use that for comparisons
+## TODO fix multiprocessing implementation
 class duplicateManager:
 
-    def __init__(self, songList, currentDir):
+    def __init__(self, songList, currentDir, useMultiProcessing):
         self.songList = songList
         self.checkedSongList = []
         self.duplicateSongList = []
         self.currentDir = currentDir
-        self.duplicatesDir = os.path.join(currentDir, 'duplicates')
         self.duplicateIndices = []
         self.removeList = []
-        ##self.usingMultiProcessing = useMultiProcessing
+        self.usingMultiProcessing = useMultiProcessing
+        self.duplicatesDir = os.path.join(currentDir, 'duplicates')
         os.makedirs(self.duplicatesDir, exist_ok=True)
 
     def checkForDuplicates(self):
-        ##if not self.usingMultiProcessing:
-        for uncheckedSong in self.songList:
-            self.isDuplicate(uncheckedSong)
-        ##elif self.usingMultiProcessing:
-        ##    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-        ##    pool.map(self.isDuplicate, self.songList)
+        if not self.usingMultiProcessing:
+            for uncheckedSong in self.songList:
+                self.isDuplicate(uncheckedSong)
+        elif self.usingMultiProcessing:
+            pool = multiprocessing.Pool(multiprocessing.cpu_count())
+            serializedSongList = pool.map(pickle.dumps, self.songList)
+            pool.map(self.isDuplicateSerialiaztionHandler, serializedSongList)
                         
                 
     def isDuplicate(self, uncheckedSong):
-        for index, song in enumerate(self.checkedSongList):
-            titleSimilarity = fuzz.token_set_ratio(uncheckedSong.title, song.title)
-            artistSimilarity = fuzz.token_set_ratio(uncheckedSong.artist, song.artist)
-            similarityIndex = artistSimilarity + titleSimilarity
-            similarityIndex = similarityIndex / 2
-            if(similarityIndex > 93 ):
-                self.duplicateIndices.append({'uncheckedSongIndex': self.songList.index(uncheckedSong), 'originalSongIndex': index})
-        self.checkedSongList.append(uncheckedSong)
+        duplicate = False
+        if len(self.checkedSongList) == 0:
+            self.checkedSongList.append(uncheckedSong)
+        else:
+            for index, song in enumerate(self.checkedSongList):
+                titleSimilarity = fuzz.token_set_ratio(uncheckedSong.title, song.title)
+                artistSimilarity = fuzz.token_set_ratio(uncheckedSong.artist, song.artist)
+                similarityIndex = artistSimilarity + titleSimilarity
+                similarityIndex = similarityIndex / 2
+                if(similarityIndex > 93 ):
+                    self.duplicateIndices.append({'uncheckedSongIndex': self.songList.index(uncheckedSong), 'originalSongIndex': index})
+                    duplicate = True
+            if not duplicate:
+                    self.checkedSongList.append(uncheckedSong)
+
+    def isDuplicateSerialiaztionHandler(self, serializedSong):
+        uncheckedSong = pickle.Unpickler(serializedSong)
+        self.isDuplicate(uncheckedSong)
 
     def handleDuplicates (self):
         for indices in self.duplicateIndices:
