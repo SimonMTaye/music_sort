b242cb7a239bcf5ae126770d399765332fe5db15
SirPotato10000 <mulat.simon@gmail.com>
2019-08-28 15:41:04 +0300

    Fixed bug with bitrate parsing

--- music_sort/metadataParser.py
+++ music_sort/metadataParser.py
@@ -7,11 +7,12 @@ def parseSong(songPath: str):
     metadata.name = basename(songPath)
     cleanMetadata(metadata)
     return metadata
-
+    
+## TODO check issue with bitrate cleaning
 def cleanMetadata(metadata):
     criticalProperties = ['artist', 'genre','album', 'albumartist']
     for property in criticalProperties:
         if  type(getattr(metadata, property, 'Unknown')) != str:
             setattr(metadata, property, 'Unknown')
-    if type(getattr(metadata, 'bitrate')) != float or type(getattr(metadata, 'bitrate')) != int: 
+    if type(getattr(metadata, 'bitrate')) != float and type(getattr(metadata, 'bitrate')) != int: 
         setattr(metadata, 'bitrate', 'Unknown')        
