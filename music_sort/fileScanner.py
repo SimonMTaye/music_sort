8cfcca50bf4c4f617f2d69344579dfb798509493
Simon Mulat <sirpotato@pop-os.localdomain>
2019-08-30 11:44:47 +0300

    Ignore 'Sorted' and 'duplicates' directory'

--- music_sort/fileScanner.py
+++ music_sort/fileScanner.py
@@ -2,29 +2,32 @@ import os
 
 wantedFiles = []
 
-def scanFolder (dir: str, wantedFileTypes: list):
+
+def scanFolder(dir: str, wantedFileTypes: list):
     filesInDir = os.scandir(dir)
     for file in filesInDir:
         if(file.is_file and correctFileType(file.name, wantedFileTypes)):
             wantedFiles.append(file.path)
-    return wantedFiles        
+    return wantedFiles
 
-def scanFolderRecursively (dir: str, wantedFileTypes: list):
+
+def scanFolderRecursively(dir: str, wantedFileTypes: list):
     try:
         stuffInDir = os.scandir(dir)
-    except (FileNotFoundError, NotADirectoryError):
+    except (FileNotFoundError, NotADirectoryError) as e:
         stuffInDir = []
-        pass  
+        print(e)
+        pass
     for thing in stuffInDir:
         if(thing.is_file and correctFileType(thing.name, wantedFileTypes)):
             wantedFiles.append(thing.path)
-        elif(thing.is_dir):
-            scanFolderRecursively(thing.path, wantedFileTypes)            
-    return wantedFiles             
+        elif(thing.is_dir and thing.name != 'duplicates' and thing.name != 'Sorted'):
+            scanFolderRecursively(thing.path, wantedFileTypes)
+    return wantedFiles
 
 
 def correctFileType(fileName: str, wantedFileTypes: list):
     for fileType in wantedFileTypes:
         if(fileName.endswith(fileType)):
             return True
-    return False  
+    return False
