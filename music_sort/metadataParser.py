a249c731475f32c1e64a6e0f2306feb9cf4d0237
Simon Mulat <sirpotato@pop-os.localdomain>
2019-08-30 11:50:22 +0300

    Formatted file

--- music_sort/metadataParser.py
+++ music_sort/metadataParser.py
@@ -1,18 +1,19 @@
 from tinytag import TinyTag
-from os.path import basename
+from os import path
 
 def parseSong(songPath: str):
     metadata = TinyTag.get(songPath)
     metadata.path = songPath
-    metadata.name = basename(songPath)
+    metadata.name = path.basename(songPath)
+    metadata.extension = path.splitext(songPath)[1]
     cleanMetadata(metadata)
     return metadata
-    
+
 ## TODO check issue with bitrate cleaning
 def cleanMetadata(metadata):
     criticalProperties = ['artist', 'genre','album', 'albumartist']
     for property in criticalProperties:
         if  type(getattr(metadata, property, 'Unknown')) != str:
             setattr(metadata, property, 'Unknown')
-    if type(getattr(metadata, 'bitrate')) != float and type(getattr(metadata, 'bitrate')) != int: 
-        setattr(metadata, 'bitrate', 'Unknown')        
+    if type(getattr(metadata, 'bitrate')) != float and type(getattr(metadata, 'bitrate')) != int:
+        setattr(metadata, 'bitrate', 'Unknown')
