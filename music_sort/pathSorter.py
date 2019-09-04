e7479a98e795837b08981416966c4b24e99c5406
Simon Mulat <sirpotato@pop-os.localdomain>
2019-09-04 17:10:51 +0300

    Fixed issue with embedded null bytes and backslash in names

--- music_sort/pathSorter.py
+++ music_sort/pathSorter.py
@@ -1,21 +1,24 @@
-import os, shutil
+import os
+import shutil
+
 
 class pathSorter:
 
     def __init__(self, sortUsing: tuple, songMetadata, initialDir, useTrackTitle):
         self.chosenAttributes = sortUsing
         for attribute in self.chosenAttributes:
-            if(attribute not in ['album', 'artist', 'genre', 'bitrate','albumartist']):
-                del self.chosenAttributes[attribute]
+            if(attribute not in ['album', 'artist', 'genre', 'bitrate', 'albumartist']):
+                raise ValueError(
+                    "Argument chosen is incorrect or not supported")
         self.selectedSong = songMetadata
         self.newDir = 'Sorted'
         for property in self.chosenAttributes:
-            self.newDir = os.path.join(self.newDir, str(getattr(self.selectedSong, property)))
-            self.checkPathValidity()
+            attributeValue = str(getattr(self.selectedSong, property))
+            attributeValue = self.legalizePathName(attributeValue)
+            self.newDir = os.path.join(self.newDir, attributeValue)
         self.newDir = os.path.normpath(self.newDir)
         self.newDir = os.path.join(initialDir, self.newDir)
         try:
-            self.checkPathValidity()
             os.makedirs(self.newDir, exist_ok=True)
         except Exception as e:
             self.newDir = os.path.join(initialDir, 'Sorted', 'Unknown')
@@ -24,13 +27,16 @@ class pathSorter:
             print(e)
             pass
         if(useTrackTitle):
-            self.newDir = os.path.join(self.newDir, self.selectedSong.title)
+            title = str(self.selectedSong.title)
+            fileName = str(title + self.selectedSong.extension)
+            fileName = self.legalizePathName(fileName)
+            self.newDir = os.path.join(self.newDir, title)
             self.newDir = os.path.normpath(self.newDir)
-            self.checkPathValidity()
         else:
-            self.newDir = os.path.join(self.newDir, self.selectedSong.name)
+            fileName = str(self.selectedSong.name)
+            fileName = self.legalizePathName(fileName)
+            self.newDir = os.path.join(self.newDir, fileName)
             self.newDir = os.path.normpath(self.newDir)
-            self.checkPathValidity()
         try:
             shutil.move(self.selectedSong.path, self.newDir)
         except OSError as e:
@@ -38,34 +44,26 @@ class pathSorter:
             print(e)
             pass
 
-
-
-    def checkPathValidity(self):
-        forbiddenCharacterList = [':', '*', '?', '"', '>', '<', '|']
+    def legalizePathName(self, pathName: str):
+        forbiddenCharacterList = [':', '*', '?', '"', '>', '<', '|', '/']
         forbiddenNameList = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3',
-        'COM4','COM5','COM6','COM7','COM8','COM9',
-        'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6',
-        'LPT7', 'LPT8', 'LPT9']
+                             'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9',
+                             'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6',
+                             'LPT7', 'LPT8', 'LPT9']
         for character in forbiddenCharacterList:
-            self.newDir = self.newDir.replace(character, '')
-        self.newDir = self.newDir.replace('\\x00', '')
-        self.newDir = self.newDir.strip()
-        name = os.path.basename(self.newDir)
-        nameWithoutExt = os.path.splitext(name)[0]
-        ext = os.path.splitext(name)[1]
-        if name in forbiddenNameList:
-            raise IllegalFileNameError('Illegal file name: ' + name)
+            pathName = pathName.replace(character, '')
+        pathName = str(pathName).strip('\x00')
+        nameWithoutExt = os.path.splitext(pathName)[0]
+        ext = os.path.splitext(pathName)[1]
+        if nameWithoutExt in forbiddenNameList:
+            raise IllegalFileNameError('Illegal file name: ' + pathName)
         if nameWithoutExt.endswith('.'):
-            name = nameWithoutExt[:len(nameWithoutExt) - 1] + ext
-            name = name.strip()
-            rootDir = os.path.split(self.newDir)[0].strip()
-            self.newDir = os.path.join(rootDir, name)
+            pathName = nameWithoutExt.strip('.') + ext
         elif ext == '.':
-            name = nameWithoutExt
-            name = name.strip()
-            rootDir = os.path.split(self.newDir)[0].strip()
-            self.newDir = os.path.join(rootDir, name)
-
+            pathName = nameWithoutExt
+        pathName = str(pathName).strip()
+        verifiedPath = pathName
+        return verifiedPath
 
 
 class IllegalFileNameError (Exception):
