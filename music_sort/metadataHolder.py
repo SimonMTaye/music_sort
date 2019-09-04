dc37cdd1f90c9146e812e61625a77928de4f0d9a
Simon Mulat <sirpotato@pop-os.localdomain>
2019-09-04 17:38:47 +0300

    dataclass for holding metadata info to simply serialization

---
+++ music_sort/metadataHolder.py
@@ -0,0 +1,17 @@
+# Template class for holding metadata
+from dataclasses import dataclass
+
+
+@dataclass
+class metadataHolder:
+    title: str
+    album: str
+    albumartist: str
+    artist: str
+    genre: str
+    bitrate: float
+    track: str
+    year: str
+    path: str
+    name: str
+    extension: str
