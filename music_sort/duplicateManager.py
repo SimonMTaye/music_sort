3842d30adb3c27fc8fb24b5437739e0badf2972d
SirPotato10000 <mulat.simon@gmail.com>
2019-11-19 17:29:17 +0300

    Refactored

--- music_sort/duplicateManager.py
+++ music_sort/duplicateManager.py
@@ -4,74 +4,77 @@ import multiprocessing
 import pickle
 from fuzzywuzzy import fuzz
 
+from metadataHolder import MetadataHolder
+
 
 # If audio fingerprinting is ever implemented, use that for comparisons
 # TODO: fix multiprocessing implementation
 class DuplicateManager:
 
-    def __init__(self, songList, currentDir):
+    def __init__(self, songList: list, currentDir: str):
         self.songList = songList
-        self.checkedSongList = []
-        self.duplicateSongList = []
         self.currentDir = currentDir
-        self.duplicateIndices = []
-        self.removeList = []
+        self.NOT_DUPLICATE = -1
+        self.FIRST_SONG = 100
+        self.SECOND_SONG = 200        
         self.duplicatesDir = os.path.join(currentDir, 'duplicates')
-        os.makedirs(self.duplicatesDir, exist_ok=True)
-
-    def checkForDuplicates(self):
-        for uncheckedSong in self.songList:
-            self.isDuplicate(uncheckedSong)
-        self.handleDuplicates()
-        self.sortDuplicates()
-        return self.checkedSongList
-
-
-    def isDuplicate(self, uncheckedSong):
-        duplicate = False
-        if len(self.checkedSongList) == 0:
-            self.checkedSongList.append(uncheckedSong)
-        else:
-            for index, song in enumerate(self.checkedSongList):
-                titleSimilarity = fuzz.token_set_ratio(
-                    uncheckedSong.title, song.title)
-                artistSimilarity = fuzz.token_set_ratio(
-                    uncheckedSong.artist, song.artist)
-                similarityIndex = artistSimilarity + titleSimilarity
-                similarityIndex = similarityIndex / 2
-                if(similarityIndex > 93):
-                    if not self.isRemix(uncheckedSong.title, song.title):
-                        self.duplicateIndices.append({'uncheckedSongIndex': self.songList.index(
-                            uncheckedSong), 'originalSongIndex': index})
-                        duplicate = True
-            if not duplicate:
-                self.checkedSongList.append(uncheckedSong)
-
-    def isRemix(self, uncheckedSongTitle: str, songTitle: str):
-        isRemix = False
-        remixDenoters = ['remix', 'acoustic', 'instrumental']
-        for denoter in remixDenoters:
-            if denoter in uncheckedSongTitle.lower():
-                isRemix = True
-            elif denoter in songTitle.lower():
-                isRemix = True
-        return isRemix
 
     def handleDuplicates(self):
-        for indices in self.duplicateIndices:
-            originalSong = self.checkedSongList[indices['originalSongIndex']]
-            duplicateSong = self.songList[indices['uncheckedSongIndex']]
-            if(originalSong.bitrate != 320 and originalSong.bitrate < duplicateSong.bitrate):
-                self.duplicateSongList.append(originalSong)
-                self.removeList.append(indices['originalSongIndex'])
-            else:
-                self.duplicateSongList.append(duplicateSong)
-        self.removeList.sort(reverse=True)
-        for item in self.removeList:
-            del self.checkedSongList[item]
+        sortedSongListDict = self.filterDuplicates(self.songList)
+        checkedList = sortedSongListDict['filteredList']
+        duplicateList = sortedSongListDict['duplicateList']
+        self.moveDuplicates(duplicateList, self.duplicatesDir)
+        return checkedList
 
-    def sortDuplicates(self):
-        for duplicate in self.duplicateSongList:
+    def filterDuplicates(self, songList: list):
+        filteredList = []
+        duplicateList = []
+        for uncheckedSong in songList:
+            duplicateIndex = self.isDuplicate(uncheckedSong, filteredList)
+            if filteredList.__len__ == 0:
+                filteredList.append(uncheckedSong)
+            elif duplicateIndex == self.NOT_DUPLICATE:
+                filteredList.append(uncheckedSong)
+            elif not duplicateIndex == self.NOT_DUPLICATE:
+                duplicateSong = songList[duplicateIndex]
+                if self.pickBetterQuality(uncheckedSong, duplicateSong) == self.FIRST_SONG:
+                    filteredList.append(uncheckedSong)
+                    duplicateList.append(duplicateSong)
+                elif self.pickBetterQuality(uncheckedSong, duplicateSong) == self.SECOND_SONG:
+                    filteredList.append(duplicateSong)
+                    duplicateList.append(uncheckedSong)
+        sortedSongListsDict = {"filteredList" : filteredList, "duplicateList" : duplicateList}             
+        return sortedSongListsDict
+
+    def isDuplicate(self, uncheckedSong: MetadataHolder, songList: list):
+        for index, song in enumerate(songList):
+            titleSimilarity = fuzz.token_set_ratio(
+                uncheckedSong.title, song.title)
+            artistSimilarity = fuzz.token_set_ratio(
+                uncheckedSong.artist, song.artist)
+            similarityIndex = artistSimilarity + titleSimilarity
+            similarityIndex = similarityIndex / 2
+            if(similarityIndex > 93):
+                if not self.isRemix(uncheckedSong.title, song.title):
+                    return index
+        return self.NOT_DUPLICATE
+           
+    def isRemix(self, firstSongTitle: str, secondSongTitle: str):
+        remixDenoters = ['remix', 'acoustic', 'instrumental']
+        for denoter in remixDenoters:
+            if denoter in firstSongTitle.lower() or denoter in secondSongTitle.lower():
+                return True
+        return False
+
+    def pickBetterQuality(self, firstSong: MetadataHolder, secondSong: MetadataHolder):
+        if secondSong.bitrate > firstSong.bitrate:
+            return self.SECOND_SONG
+        else:
+            return self.FIRST_SONG
+            
+    def moveDuplicates(self, duplicateSongList: list, duplicatesDir: str):
+        os.makedirs(duplicatesDir, exist_ok=True)
+        for duplicate in duplicateSongList:
             try:
                 shutil.copy2(duplicate.path, self.duplicatesDir)
                 os.remove(duplicate.path)
